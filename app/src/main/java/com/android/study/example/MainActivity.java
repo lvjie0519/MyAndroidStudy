52b3c7e527d7bb0437d8d6a66d26d46de6c88aca
lvjie <991102@nd.com>
2018-10-24 16:59:06 +0800

    ok http

--- app/src/main/java/com/android/study/example/MainActivity.java
+++ app/src/main/java/com/android/study/example/MainActivity.java
@@ -8,14 +8,30 @@ import com.android.study.example.butterknife.ButterKnifeTestActivity;
 import com.android.study.example.butterknife.MyButterKnifeTestActivity;
 import com.annotaions.example.MyAnnotation;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 @MyAnnotation("hello world")
 public class MainActivity extends AppCompatActivity {
+
+    private OkHttpClient mClient;
+    private Request mRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initData();
         initView();
     }
 
@@ -33,6 +49,72 @@ public class MainActivity extends AppCompatActivity {
                 MyButterKnifeTestActivity.startActivity(MainActivity.this);
             }
         });
+
+        // okhttp 同步
+        findViewById(R.id.btn_okhttp_sync).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                synOkHttpTest();
+            }
+        });
+
+        // okhttp 异步
+        findViewById(R.id.btn_okhttp_sync).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                asynOkHttpTest();
+            }
+        });
+    }
+
+    private void initData(){
+        mClient = new OkHttpClient.Builder()
+                .cache(new Cache(new File("cache"), 24*1024))
+                .build();
+        mRequest = new Request.Builder().url("http://www.baidu.com").get().build();
+    }
+
+    private void synOkHttpTest(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Call mCall = mClient.newCall(mRequest);
+                try {
+                    Response response = mCall.execute();
+                    System.out.println(response.body().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void asynOkHttpTest(){
+        System.out.println(Thread.currentThread().getName());
+        Call mCall = mClient.newCall(mRequest);
+        mCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println(e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // 在子线程中执行， 线程名为：OkHttp http://www.baidu.com/
+                System.out.println(Thread.currentThread().getName());
+                System.out.println(response.body().string());
+            }
+        });
+
+    }
+
+
+    class A implements Interceptor{
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            return null;
+        }
     }
 
 }
