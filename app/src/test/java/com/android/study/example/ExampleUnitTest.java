f0b14d5122cc127b757bc3802cf15ac97cf2ee09
lvjie1 <lvjie1@xiaomi.com>
2019-06-10 16:39:43 +0800

    add test

--- app/src/test/java/com/android/study/example/ExampleUnitTest.java
+++ app/src/test/java/com/android/study/example/ExampleUnitTest.java
@@ -13,5 +13,9 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+        String action = "issue_mifare", type="1", product_id="66666-00211", source_channel=null;
+        String url = "https://tsmclient.miui.com?action=%1$s&type=%2$s&product_id=%3$s&source_channel=%4$s";
+        System.out.println(String.format(url, action, type, product_id, source_channel));
     }
 }
