3bec18492b0b53508f78e760524dff8eb1d337f7
lvjie1 <lvjie1@xiaomi.com>
2019-06-22 22:11:58 +0800

    MediaPlayer播放AudioRecord录制的音频，还有点问题

--- app/src/main/java/com/android/study/example/androidapi/AudioDemoTestActivity.java
+++ app/src/main/java/com/android/study/example/androidapi/AudioDemoTestActivity.java
@@ -22,6 +22,8 @@ import com.tbruyelle.rxpermissions.RxPermissions;
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -72,7 +74,7 @@ public class AudioDemoTestActivity extends AppCompatActivity {
 
     private void initData(){
         recordFileName = getFilesDir()+File.separator+"aa.mp3";
-        audioRecordFileName = getFilesDir()+File.separator+"bb.mp3";
+        audioRecordFileName = getFilesDir()+File.separator+"bb.raw";
         rxPermissions = RxPermissions.getInstance(this);
     }
 
@@ -151,6 +153,12 @@ public class AudioDemoTestActivity extends AppCompatActivity {
         }
 //        File file = new File(getFilesDir().getPath()+File.separator+"lovewholelife.mp3");
         File file = new File(recordFileName);
+
+        // 播放AudioRecordl录制的音频,还有问题
+//        String newFileName = getFilesDir()+File.separator+"bbb.raw";
+//        copyWaveFile(audioRecordFileName, newFileName);
+//        File file = new File(newFileName);
+
 
         try {
             if(file.exists()){
@@ -332,6 +340,9 @@ public class AudioDemoTestActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 之所以使用同步方法，是因为防止多次点击录音，录音在写文件的过程中，是在子线程中进行的，需要考虑线程的同步
+     */
     private synchronized void stopAudioRecord() {
 
         // 结束录音
@@ -347,6 +358,106 @@ public class AudioDemoTestActivity extends AppCompatActivity {
         }
 
     }
+
+    /**
+     * MediaPlayer 是不能直接播放 AudioRecorder的录音的音频文件的，需要经过转换
+     * @param inFileName
+     * @param outFileName
+     */
+    private void copyWaveFile(String inFileName, String outFileName) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long sampleRateInHz = 9500;
+        long longSampleRate = sampleRateInHz;
+        int channels = 2;
+        long byteRate = 16 * sampleRateInHz * channels / 8;
+
+        byte[] data = new byte[1024];
+        try
+        {
+            in = new FileInputStream(inFileName);
+            out = new FileOutputStream(outFileName);
+
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen, longSampleRate, channels, byteRate);
+            while(in.read(data) != -1)
+            {
+                out.write(data);
+            }
+
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 这里提供一个头信息。插入这些信息就可以得到可以播放的文件。
+     * 为我为啥插入这44个字节，这个还真没深入研究，不过你随便打开一个wav
+     * 音频的文件，可以发现前面的头文件可以说基本一样哦。每种格式的文件都有
+     * 自己特有的头文件。
+     */
+    private void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+                                     long totalDataLen, long longSampleRate, int channels, long byteRate)
+            throws IOException {
+        byte[] header = new byte[44];
+        header[0] = 'R'; // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f'; // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1; // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8); // block align
+        header[33] = 0;
+        header[34] = 16; // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+        out.write(header, 0, 44);
+    }
+
 
 
 }
