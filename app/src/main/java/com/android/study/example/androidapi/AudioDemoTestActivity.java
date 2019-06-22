c44dfa2bb9c68263ee42d1c146808d6f7bec852b
lvjie1 <lvjie1@xiaomi.com>
2019-06-22 11:58:37 +0800

    add AudioRecord demo

--- app/src/main/java/com/android/study/example/androidapi/AudioDemoTestActivity.java
+++ app/src/main/java/com/android/study/example/androidapi/AudioDemoTestActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Handler;
@@ -17,9 +19,13 @@ import android.widget.TextView;
 import com.android.study.example.R;
 import com.tbruyelle.rxpermissions.RxPermissions;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.concurrent.ThreadLocalRandom;
 
 import rx.functions.Action1;
 
@@ -34,6 +40,11 @@ public class AudioDemoTestActivity extends AppCompatActivity {
     private String recordFileName = "";
     private MediaRecorder mRecorder;
     private RxPermissions rxPermissions;
+
+    // AudioRecord  录音文件
+    private String audioRecordFileName = "";
+    private AudioRecord mAudioRecord;
+    private boolean mIsAudioRecording = false;
 
     private Handler mHandler = new Handler(){
         @Override
@@ -61,6 +72,7 @@ public class AudioDemoTestActivity extends AppCompatActivity {
 
     private void initData(){
         recordFileName = getFilesDir()+File.separator+"aa.mp3";
+        audioRecordFileName = getFilesDir()+File.separator+"bb.mp3";
         rxPermissions = RxPermissions.getInstance(this);
     }
 
@@ -81,7 +93,31 @@ public class AudioDemoTestActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.btn_audio_start_audio).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_audio_start_media_recorder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 开始录音, 需要动态获取权限
+                rxPermissions.request(Manifest.permission.RECORD_AUDIO).subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean granted) {
+                        if(granted){
+                            startMediaRecord();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        findViewById(R.id.btn_audio_stop_media_recorder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 结束录音
+                stopMediaRecord();
+            }
+        });
+
+        findViewById(R.id.btn_audio_start_audio_recorder).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 开始录音, 需要动态获取权限
@@ -97,7 +133,7 @@ public class AudioDemoTestActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.btn_audio_stop_audio).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_audio_stop_audio_recorder).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 结束录音
@@ -170,7 +206,7 @@ public class AudioDemoTestActivity extends AppCompatActivity {
         }
     }
 
-    private void startAudioRecord(){
+    private void startMediaRecord(){
         // 开始录音
         Log.i("lvjie", "recordFileName="+recordFileName);
         File audioFile = new File(recordFileName);
@@ -214,14 +250,102 @@ public class AudioDemoTestActivity extends AppCompatActivity {
 
     }
 
-    private void stopAudioRecord(){
+    private void stopMediaRecord(){
         // 结束录音
-        Log.i("lvjie", "stopAudioRecord...");
+        Log.i("lvjie", "stopMediaRecord...");
         if(mRecorder != null){
             mRecorder.stop();
             mRecorder.release();
             mRecorder = null;
         }
+    }
+
+
+    private void startAudioRecord() {
+
+        // 开始录音
+        Log.i("lvjie", "recordFileName="+audioRecordFileName);
+        File audioFile = new File(audioRecordFileName);
+        if(audioFile.exists()){
+            audioFile.delete();
+        }
+        try {
+            audioFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        stopAudioRecord();
+
+        // 音频源：指的是从哪里采集音频。这里我们当然是从麦克风采集音频，所以此参数的值为MIC
+        int audioSource = MediaRecorder.AudioSource.MIC;
+        // 采样率：音频的采样频率，每秒钟能够采样的次数，采样率越高，音质越高。
+        int sampleRateInHz = 9500;
+        // 声道设置：android支持双声道立体声和单声道。MONO单声道，STEREO立体声
+        int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+        // 编码制式和采样大小   android支持的采样大小16bit 或者8bit。当然采样大小越大，那么信息量越多，音质也越高，现在主流的采样大小都是16bit，在低质量的语音传输的时候8bit足够了。
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+        // 采集数据需要的缓冲区的大小，如果不知道最小需要的大小可以在getMinBufferSize()查看。
+        final int bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat);
+
+        if(bufferSizeInBytes == AudioRecord.ERROR_BAD_VALUE){
+            // channelConfig错误或sampleRateInHz错误
+            Log.i("lvjie", "channelConfig错误或sampleRateInHz错误");
+            return;
+        }else if(bufferSizeInBytes == AudioRecord.ERROR){
+            // sampleRateInHz错误
+            Log.i("lvjie", "sampleRateInHz错误");
+            return;
+        }
+
+        // int audioSource, int sampleRateInHz, int channelConfig, int audioFormat,int bufferSizeInBytes
+        mAudioRecord = new AudioRecord(audioSource, sampleRateInHz, channelConfig, audioFormat, bufferSizeInBytes);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                DataOutputStream outputStream = null;
+                try {
+                    outputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(audioRecordFileName)));
+                    byte[] buffer = new byte[bufferSizeInBytes];
+                    //开始录音
+                    mAudioRecord.startRecording();
+                    mIsAudioRecording = true;
+                    while (mIsAudioRecording) {
+                        int readResult = mAudioRecord.read(buffer, 0, buffer.length);
+                        for (int i = 0; i < readResult; i++) {
+                            outputStream.write(buffer[i]);
+                        }
+                        Log.i("lvjie", Thread.currentThread().getName()+"  录制中....");
+                    }
+                    Log.i("lvjie", Thread.currentThread().getName()+"  录制结束....");
+                    stopAudioRecord();
+                    outputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+
+    }
+
+    private synchronized void stopAudioRecord() {
+
+        // 结束录音
+        Log.i("lvjie", Thread.currentThread().getName()+"  stopAudioRecord...mIsAudioRecording="+mIsAudioRecording);
+
+        if(mIsAudioRecording){
+            mIsAudioRecording = false;
+            if(mAudioRecord != null){
+                mAudioRecord.stop();
+                mAudioRecord.release();
+                mAudioRecord = null;
+            }
+        }
+
     }
 
 
