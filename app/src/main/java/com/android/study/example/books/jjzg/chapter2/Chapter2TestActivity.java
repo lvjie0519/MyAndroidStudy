c10bc096086e85ec0bbfb5ded463f15b190c1b18
lvjie1 <lvjie1@xiaomi.com>
2019-09-06 19:20:06 +0800

    add chapter2

--- app/src/main/java/com/android/study/example/books/jjzg/chapter2/Chapter2TestActivity.java
+++ app/src/main/java/com/android/study/example/books/jjzg/chapter2/Chapter2TestActivity.java
@@ -2,12 +2,17 @@ package com.android.study.example.books.jjzg.chapter2;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.study.example.R;
+import com.android.study.example.utils.ToastUtil;
 
 public class Chapter2TestActivity extends AppCompatActivity {
+
+    private View snackbarContent;
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context, Chapter2TestActivity.class);
@@ -18,5 +23,31 @@ public class Chapter2TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charpter2_test);
+
+        initView();
+    }
+
+    private void initView(){
+        snackbarContent = findViewById(R.id.layout_snackbar_content);
+    }
+
+    /*****************   onClick begin *********************/
+    public void snackbarOnClick(View view){
+        showSnackbar();
+    }
+
+    /*****************   onClick end *********************/
+
+
+
+    private void showSnackbar(){
+        Snackbar.make(snackbarContent, "标题", Snackbar.LENGTH_LONG)
+                .setAction("点击事件", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ToastUtil.showToast(Chapter2TestActivity.this, "点击 Snackbar");
+                    }
+                }).setDuration(Snackbar.LENGTH_LONG)
+                .show();
     }
 }
