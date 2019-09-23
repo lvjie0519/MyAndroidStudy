933853e7dc6308b40ac7dd1748fc2ad6c7478f1f
lvjie1 <lvjie1@xiaomi.com>
2019-09-23 11:33:31 +0800

    属性动画-移动组合

--- app/src/main/java/com/android/study/example/uidemo/animation/AnimationDemoActivity.java
+++ app/src/main/java/com/android/study/example/uidemo/animation/AnimationDemoActivity.java
@@ -1,5 +1,6 @@
 package com.android.study.example.uidemo.animation;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,14 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import com.android.study.example.R;
+import com.android.study.example.utils.ToastUtil;
 
 public class AnimationDemoActivity extends AppCompatActivity {
 
     private View scaleAnimView;
+
+    private ObjectAnimator objectAnimatorX;
+    private ObjectAnimator objectAnimatorY;
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context, AnimationDemoActivity.class);
@@ -24,12 +29,44 @@ public class AnimationDemoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_demo);
 
-        startScaleAnimation();
+        initView();
     }
 
+    private void initView(){
+        scaleAnimView = findViewById(R.id.view_anim);
+        findViewById(R.id.btn_anim).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPopsAnimTrans();
+            }
+        });
+
+        scaleAnimView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.showToast(AnimationDemoActivity.this, "我被点击了...");
+            }
+        });
+    }
+
+    // 区间动画-缩放
     private void startScaleAnimation(){
-        scaleAnimView = findViewById(R.id.view_scale_anim);
         Animation animation = AnimationUtils.loadAnimation(this,R.anim.scale_animation);
         scaleAnimView.startAnimation(animation);
+    }
+
+    // 属性动画-平移
+    private void startPopsAnimTrans(){
+        if(objectAnimatorX == null){
+            float [] x= {0f,60f,120f, 240f};
+            float [] y= {0f,30f,220f,90f};
+//            objectAnimatorX = ObjectAnimator.ofFloat(scaleAnimView,"alpha", 1f, 0.5f, 1f);
+            objectAnimatorX = ObjectAnimator.ofFloat(scaleAnimView,"translationX", x);
+            objectAnimatorX.setDuration(2000);
+            objectAnimatorY = ObjectAnimator.ofFloat(scaleAnimView,"translationY", y);
+            objectAnimatorY.setDuration(2000);
+        }
+        objectAnimatorX.start();
+        objectAnimatorY.start();
     }
 }
