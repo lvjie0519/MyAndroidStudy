e6ad3ccff6a3f0938c9f00010d8126e1a95aa9e7
lvjie1 <lvjie1@xiaomi.com>
2020-10-26 20:15:03 +0800

    trace view 的使用

--- app/src/main/java/com/android/study/example/performance_optimization/TraceViewUseTestActivity.java
+++ app/src/main/java/com/android/study/example/performance_optimization/TraceViewUseTestActivity.java
@@ -2,8 +2,11 @@ package com.android.study.example.performance_optimization;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.android.study.example.R;
 
@@ -18,5 +21,35 @@ public class TraceViewUseTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trace_view_use_test);
+    }
+
+    public void onClickFuncTimeCost(View view) {
+        /**
+         * 1、打开 Android studio Profiler
+         * 2、添加startMethodTracing 和 stopMethodTracing代码；
+         * 3、当点击按钮时， 会看到自动生成一个文件， 会显示api的调用及耗时；
+         */
+        Debug.startMethodTracing("lvjie.trace");
+        Log.i("lvjie", "startMethodTracing...");
+        func1();
+        func2();
+        Debug.stopMethodTracing();
+        Log.i("lvjie", "stopMethodTracing...");
+    }
+
+    public void func1(){
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        func2();
+    }
+    public void func2(){
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
