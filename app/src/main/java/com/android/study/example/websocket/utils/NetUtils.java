0b9334d69d6e687910ceaff6be9e2594a15828b9
lvjie <lvjie@mgtv.com>
2023-04-27 17:36:13 +0800

    获取本地ip

--- app/src/main/java/com/android/study/example/websocket/utils/NetUtils.java
+++ app/src/main/java/com/android/study/example/websocket/utils/NetUtils.java
@@ -1,10 +1,21 @@
 package com.android.study.example.websocket.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class NetUtils {
     private final static String TAG = "NetUtils";
@@ -40,5 +51,43 @@ public class NetUtils {
             }
         }
         return retVal;
+    }
+
+    public static String getLocAddress() {
+        String ipaddress = "";
+
+        try {
+            Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            // 遍历所用的网络接口
+            while (en.hasMoreElements()) {
+                NetworkInterface networks = en.nextElement();
+                // 得到每一个网络接口绑定的所有ip
+                Enumeration<InetAddress> address = networks.getInetAddresses();
+                // 遍历每一个接口绑定的所有ip
+                while (address.hasMoreElements()) {
+                    InetAddress ip = address.nextElement();
+                    if (!ip.isLoopbackAddress() && isIPv4Address(ip.getHostAddress())) {
+
+                        ipaddress = ip.getHostAddress();
+                        Log.i(TAG, "本机IP: "+ipaddress);
+                        return ipaddress;
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.e("", "获取本地ip地址失败");
+            ipaddress = "";
+            e.printStackTrace();
+        }
+
+        Log.i(TAG, "本机IP:" + ipaddress);
+        return ipaddress;
+    }
+
+    public static boolean isIPv4Address(String ip) {
+        if(TextUtils.isEmpty(ip)){
+            return false;
+        }
+        return ip.split("\\.").length == 4;
     }
 }
