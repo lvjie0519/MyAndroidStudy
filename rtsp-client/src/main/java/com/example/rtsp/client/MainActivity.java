ed16db00094e2fbcc20e174337c6ea9cbd7b9e36
lvjie <lvjie@mgtv.com>
2023-06-09 11:38:13 +0800

    完成 rstp client 编写， 能在高版本手机安装

--- rtsp-client/src/main/java/com/example/rtsp/client/MainActivity.java
+++ rtsp-client/src/main/java/com/example/rtsp/client/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.rtsp.client;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private VideoView mVideoView;
+    private MediaController mMediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVideoView = findViewById(R.id.video_view);
+
+        mMediaController = new MediaController(this);
+        mMediaController.setAnchorView(mVideoView);
+        mVideoView.setMediaController(mMediaController);
+        mVideoView.setVideoURI(Uri.parse("rtsp://192.168.3.93:1234"));
+        mVideoView.requestFocus();
+        mVideoView.start();
+        Log.i(TAG, "start");
     }
 }
