6509fb54c0786118fcb5b9695e9fb431a75ceb1f
lvjie <lvjie@mgtv.com>
2023-06-15 11:29:04 +0800

    1、添加websocket、zxing二维码开源库； 2、实现扫码能力

---
+++ rtsp-server/src/main/java/com/example/rtsp/server/CaptureActivity.java
@@ -0,0 +1,62 @@
+package com.example.rtsp.server;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+public class CaptureActivity extends AppCompatActivity {
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+
+        barcodeScannerView = (DecoratedBarcodeView) findViewById(R.id.dbv_custom);
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+}
--- rtsp-server/src/main/java/com/example/rtsp/server/MainActivity.java
+++ rtsp-server/src/main/java/com/example/rtsp/server/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.rtsp.server;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,5 +47,30 @@ public class MainActivity extends AppCompatActivity {
 
         layout.addView(view1);
         layout.addView(view2);
+    }
+
+    public void onClickOpenQrPage(View view) {
+        new IntentIntegrator(MainActivity.this)
+                .setCaptureActivity(CaptureActivity.class)
+                .setOrientationLocked(false)
+                .setPrompt("请对准二维码")  // 设置提示语
+                .setCameraId(0)  // 选择摄像头,可使用前置或者后置
+                .setBeepEnabled(true)  // 是否开启声音,扫完码之后会"哔"的一声
+                .setBarcodeImageEnabled(true)  // 扫完码之后生成二维码的图片
+                .initiateScan();  // 初始化扫码
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
