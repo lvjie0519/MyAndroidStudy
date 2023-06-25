01e52e5b4d7a8820eca888a6f62e6c10f075e210
lvjie <lvjie@mgtv.com>
2023-06-25 11:32:55 +0800

    新增Google开业 libyuv 库

---
+++ libyuv/src/main/java/com/libyuv/util/YuvUtil.java
@@ -0,0 +1,109 @@
+package com.libyuv.util;
+
+/**
+ * 作者：请叫我百米冲刺 on 2017/8/28 上午11:05
+ * 邮箱：mail@hezhilin.cc
+ */
+
+public class YuvUtil {
+
+    static {
+        System.loadLibrary("yuvutil");
+    }
+
+    /**
+     * 初始化
+     *
+     * @param width      原始的宽
+     * @param height     原始的高
+     * @param dst_width  输出的宽
+     * @param dst_height 输出的高
+     **/
+    public static native void init(int width, int height, int dst_width, int dst_height);
+
+    public static void rotateNV21(byte[] input, byte[] output, int width, int height, int rotation) {
+        if (rotation==0){
+            System.arraycopy(input, 0, output, 0, input.length);
+            return;
+        }
+        boolean swap = (rotation == 90 || rotation == 270);
+        boolean yflip = (rotation == 90 || rotation == 180);
+        boolean xflip = (rotation == 270 || rotation == 180);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int xo = x, yo = y;
+                int w = width, h = height;
+                int xi = xo, yi = yo;
+                if (swap) {
+                    xi = w * yo / h;
+                    yi = h * xo / w;
+                }
+                if (yflip) {
+                    yi = h - yi - 1;
+                }
+                if (xflip) {
+                    xi = w - xi - 1;
+                }
+                output[w * yo + xo] = input[w * yi + xi];
+                int fs = w * h;
+                int qs = (fs >> 2);
+                xi = (xi >> 1);
+                yi = (yi >> 1);
+                xo = (xo >> 1);
+                yo = (yo >> 1);
+                w = (w >> 1);
+                h = (h >> 1);
+                // adjust for interleave here
+                int ui = fs + (w * yi + xi) * 2;
+                int uo = fs + (w * yo + xo) * 2;
+                // and here
+                int vi = ui + 1;
+                int vo = uo + 1;
+                output[uo] = input[ui];
+                output[vo] = input[vi];
+            }
+        }
+    }
+
+
+    /**
+     * YUV数据的基本的处理
+     *
+     * @param src        原始数据
+     * @param width      原始的宽
+     * @param height     原始的高
+     * @param dst        输出数据
+     * @param dst_width  输出的宽
+     * @param dst_height 输出的高
+     * @param mode       压缩模式。这里为0，1，2，3 速度由快到慢，质量由低到高，一般用0就好了，因为0的速度最快
+     * @param degree     旋转的角度，90，180和270三种
+     * @param isMirror   是否镜像，一般只有270的时候才需要镜像
+     **/
+    public static native void compressYUV(byte[] src, int width, int height, byte[] dst, int dst_width, int dst_height, int mode, int degree, boolean isMirror);
+
+    public static native void mirrorYUV(byte[] src, int width, int height, byte[] dst, int dst_width, int dst_height, int mode, int degree);
+
+    /**
+     * yuv数据的裁剪操作
+     *
+     * @param src        原始数据
+     * @param width      原始的宽
+     * @param height     原始的高
+     * @param dst        输出数据
+     * @param dst_width  输出的宽
+     * @param dst_height 输出的高
+     * @param left       裁剪的x的开始位置，必须为偶数，否则显示会有问题
+     * @param top        裁剪的y的开始位置，必须为偶数，否则显示会有问题
+     **/
+    public static native void cropYUV(byte[] src, int width, int height, byte[] dst, int dst_width, int dst_height, int left, int top);
+
+    /**
+     * 将I420转化为NV21
+     *
+     * @param i420Src 原始I420数据
+     * @param nv21Src 转化后的NV21数据
+     * @param width   输出的宽
+     * @param width   输出的高
+     **/
+    public static native void yuvI420ToNV21(byte[] i420Src, byte[] nv21Src, int width, int height);
+}
