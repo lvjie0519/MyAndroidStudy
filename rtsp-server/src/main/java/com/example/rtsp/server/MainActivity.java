4f844f207fc9cac97df7d10fa3e03d24610a7adc
lvjie <lvjie@mgtv.com>
2023-07-14 08:54:38 +0800

    修改demo

--- rtsp-server/src/main/java/com/example/rtsp/server/MainActivity.java
+++ rtsp-server/src/main/java/com/example/rtsp/server/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addView();
+//        addView();
     }
 
     public void onClickOpenRstpServerPage(View view) {
