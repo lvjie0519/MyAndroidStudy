bc720247d710a30c3b6bc96b32636aef1240524a
lvjie <lvjie@qq.com>
2023-07-30 22:28:59 +0800

    webview 支持播放器全屏

--- mymgstudyapp/src/main/java/com/example/mymgstudyapp/webview/WebViewTestActivity.java
+++ mymgstudyapp/src/main/java/com/example/mymgstudyapp/webview/WebViewTestActivity.java
@@ -1,25 +1,34 @@
 package com.example.mymgstudyapp.webview;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 
 import com.example.mymgstudyapp.R;
 
 public class WebViewTestActivity extends AppCompatActivity {
 
+    private FrameLayout mFrameLayout;
     private WebView mWebView;
 
     public static void startActivity(Context context){
@@ -29,10 +38,17 @@ public class WebViewTestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view_test);
 
-        initWebView();
+        initView();
+    }
+
+    private void initView(){
+        this.mFrameLayout = findViewById(R.id.framelayout);
+        this.initWebView();
     }
 
     private void initWebView(){
@@ -41,6 +57,9 @@ public class WebViewTestActivity extends AppCompatActivity {
 
         //支持javascript
         mWebView.getSettings().setJavaScriptEnabled(true);
+//        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+//        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+
 
         //缩放操作
         mWebView.getSettings().setSupportZoom(true);  // 支持缩放，默认为true。是下面那个的前提。
@@ -59,50 +78,61 @@ public class WebViewTestActivity extends AppCompatActivity {
         mWebView.requestFocus();
         mWebView.getSettings().setLoadWithOverviewMode(true);
         mWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        mWebView.loadUrl("file:///android_asset/upload_image.html");
+//        mWebView.loadUrl("file:///android_asset/upload_image.html");
+        mWebView.loadUrl("https://haokan.baidu.com/v?pd=wisenatural&vid=21794045913693680");
 
-        this.mWebView.setWebViewClient(new WebViewClient(){
+//        this.mWebView.setWebViewClient(new InnerWebViewClient());
 
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                Log.i("lvjie", "onPageStarted ...");
-
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                Log.i("lvjie", "onPageFinished ...");
-
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Log.i("lvjie", "shouldOverrideUrlLoading ..."+url);
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
-                Log.i("lvjie", "onReceivedError ...WebResourceError: "+error.toString());
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                super.onReceivedError(view, errorCode, description, failingUrl);
-                Log.i("lvjie", "onReceivedError ...failingUrl: "+failingUrl);
-            }
-
-            @Nullable
-            @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-                return super.shouldInterceptRequest(view, request);
-            }
-        });
+        this.mWebView.setWebChromeClient(new InsideWebChromeClient());
 
     }
 
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        switch (newConfig.orientation) {
+            case Configuration.ORIENTATION_LANDSCAPE:
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                break;
+            case Configuration.ORIENTATION_PORTRAIT:
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                break;
+        }
+    }
+
+    private class InsideWebChromeClient extends WebChromeClient {
+        private View mCustomView;
+        private CustomViewCallback mCustomViewCallback;
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            super.onShowCustomView(view, callback);
+            if (mCustomView != null) {
+                callback.onCustomViewHidden();
+                return;
+            }
+            mCustomView = view;
+            mFrameLayout.addView(mCustomView);
+            mCustomViewCallback = callback;
+            mWebView.setVisibility(View.GONE);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+
+        public void onHideCustomView() {
+            mWebView.setVisibility(View.VISIBLE);
+            if (mCustomView == null) {
+                return;
+            }
+            mCustomView.setVisibility(View.GONE);
+            mFrameLayout.removeView(mCustomView);
+            mCustomViewCallback.onCustomViewHidden();
+            mCustomView = null;
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            super.onHideCustomView();
+        }
+    }
 
 }
